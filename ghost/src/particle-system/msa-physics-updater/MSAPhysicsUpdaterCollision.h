a3b74716801c636e52d5fe6880c96c83dfa269dc
Thomas Eberwein <tommi@thomastraum.com>
2012-06-14 12:02:26 +0100

    fixed pointer issue in msalib. fixed bounding box code

--- ghost/src/particle-system/msa-physics-updater/MSAPhysicsUpdaterCollision.h
+++ ghost/src/particle-system/msa-physics-updater/MSAPhysicsUpdaterCollision.h
@@ -19,48 +19,6 @@ class MSAPhysicsUpdaterCollision : public Physics::ParticleUpdater3D {
     Vec3f min;
     Vec3f max;
     
-    
-private:
-    
-    
-    bool checkCollisionBetween( Physics::Particle3D * a, Physics::Particle3D * b )
-    {
-        if( a==b ) return false;
-        if (a->isFixed()) return false;
-        // infinite creation of new ones later if they all collidewith each other //
-        if (a->hasCollision() == false ) return false;
-        
-        
-//        Vec3f a_pos = a->getPosition();
-//        if (a_pos.x > max.x || a_pos.x < min.x  ) return false;
-//        if (a_pos.y > max.y || a_pos.y < min.y  ) return false;
-//        if (a_pos.z > max.z || a_pos.z < min.z  ) return false;
-        
-        
-        if((a->collisionPlane & b->collisionPlane) == 0) {
-            return false;
-        }
-//        printf("same planes %i %i\n", a->collisionPlane, b->collisionPlane);
-        
-        float restLength = b->getRadius() + a->getRadius();
-        Vec3f delta = b->getPosition() - a->getPosition();
-        float deltaLength2 = delta.lengthSquared();
-        if(deltaLength2 >restLength * restLength) return false;
-        
-        float deltaLength = sqrt(deltaLength2);	// TODO: fast approximation of square root (1st order Taylor-expansion at a neighborhood of the rest length r (one Newton-Raphson iteration with initial guess r))
-        float force = (deltaLength - restLength) / (deltaLength * (a->getInvMass() + b->getInvMass()));
-        
-        Vec3f deltaForce = delta * force;
-        
-        if (a->isFree()) a->moveBy(deltaForce * a->getInvMass(), false);
-//        if (b->isFree()) b->moveBy(deltaForce * -b->getInvMass(), false);
-        
-//        a->collidedWithParticle(b, deltaForce);
-//        b->collidedWithParticle(a, -deltaForce);
-        
-        return true;
-    }
-    
 public:
     
     MSAPhysicsUpdaterCollision() {
@@ -79,7 +37,7 @@ public:
                 Vec3f position = _pA->getPosition();
                 CollisionEvent e = CollisionEvent( ofVec3f(position.x,position.y,position.z) ); //, _pA, pB );
                 ofNotifyEvent( CollisionEventDispatcher, e );
-//                _pA->kill();
+                _pA->kill();
                 break;
             }
             it++;
@@ -89,23 +47,31 @@ public:
     
     void calculateBoundingBox()
     {
-//        Vec3f c_pos;
-//        vector<Physics::Particle3D*>::iterator it = colliders.begin();
-//        while( it != colliders.end() ) {
-//            Physics::Particle3D * c = *it;
-//            c_pos = c->getPosition();
-//            
-//            if ( c_pos.x < min.x ) min.x = c_pos.x;
-//            if ( c_pos.y < min.y ) min.y = c_pos.y;
-//            if ( c_pos.z < min.z ) min.z = c_pos.z;
-//            
-//            if ( c_pos.x > max.x ) max.x = c_pos.x;
-//            if ( c_pos.y > max.y ) max.y = c_pos.y;
-//            if ( c_pos.z > max.z ) min.z = c_pos.z;
-//            
-//            
-//            it++;
-//        }
+        Vec3f c_pos;
+        Vec3f radius;
+        
+        Vec3f newmin;
+        Vec3f newmax;
+        
+        vector<Physics::Particle3D*>::iterator it = colliders.begin();
+        while( it != colliders.end() ) {
+            Physics::Particle3D * c = *it;
+            c_pos = c->getPosition();
+            radius = Vec3f( c->getRadius(), c->getRadius(), c->getRadius() );
+            
+            newmin = c_pos - radius;
+            newmax = c_pos + radius;
+            
+            if ( newmin.x < min.x ) min.x = newmin.x;
+            if ( newmin.y < min.y ) min.y = newmin.y;
+            if ( newmin.z < min.z ) min.z = newmin.z;
+            
+            if ( newmax.x > max.x ) max.x = newmax.x;
+            if ( newmax.y > max.y ) max.y = newmax.y;
+            if ( newmax.z > max.z ) max.z = newmax.z;
+            
+            it++;
+        }
     }
     
     void addToCollisionCheck( TT_Custom_MSAParticle3D * _p )
@@ -121,6 +87,47 @@ public:
     void checkCollisionWithThese( vector<TT_Custom_MSAParticle3D * > * _colliders )
     {
 //        colliders = _colliders;
+    }
+    
+private:
+    
+    bool checkCollisionBetween( Physics::Particle3D * a, Physics::Particle3D * b )
+    {
+        
+        if( a==b ) return false;
+        if (a->isFixed()) return false;
+        // only collide the ones which have collision enabled.
+        if (a->hasCollision() == false ) return false;
+        
+        // check with bounding box
+        Vec3f a_pos = a->getPosition();
+        if (a_pos.x > max.x || a_pos.x < min.x  ) return false;
+        if (a_pos.y > max.y || a_pos.y < min.y  ) return false;
+        if (a_pos.z > max.z || a_pos.z < min.z  ) return false;
+        
+        
+        if((a->collisionPlane & b->collisionPlane) == 0) {
+            return false;
+        }
+        //        printf("same planes %i %i\n", a->collisionPlane, b->collisionPlane);
+        
+        float restLength = b->getRadius() + a->getRadius();
+        Vec3f delta = b->getPosition() - a_pos;
+        float deltaLength2 = delta.lengthSquared();
+        if(deltaLength2 >restLength * restLength) return false;
+        
+        float deltaLength = sqrt(deltaLength2);	// TODO: fast approximation of square root (1st order Taylor-expansion at a neighborhood of the rest length r (one Newton-Raphson iteration with initial guess r))
+        float force = (deltaLength - restLength) / (deltaLength * (a->getInvMass() + b->getInvMass()));
+        
+        Vec3f deltaForce = delta * force;
+        
+        if (a->isFree()) a->moveBy(deltaForce * a->getInvMass(), false);
+        //        if (b->isFree()) b->moveBy(deltaForce * -b->getInvMass(), false);
+        
+        //        a->collidedWithParticle(b, deltaForce);
+        //        b->collidedWithParticle(a, -deltaForce);
+        
+        return true;
     }
 };
 
